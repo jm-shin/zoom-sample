bb32078fed5bf53177f2975c3aa25ba22b24da4a
jongmin <ace4463@gmail.com>
2021-11-11 18:36:16 +0900

    add websocket chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,36 @@
-alert('hi');
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nickname');
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+    console.log('서버 연결 완료');
+});
+
+socket.addEventListener('message', (message) => {
+    //console.log('New message:', message.data);
+    const li = document.createElement('li');
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener('close', () => {
+    console.log('서버 연결 종료');
+});
+
+function handleSubmit (event) {
+    event.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(input.value);
+    input.value = '';
+}
+
+function handleNickSubmit (event) {
+        event.preventDefault();
+        const input = nickForm.querySelector('input');
+        socket.send(input.value);
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -14,5 +16,33 @@ app.get('/*', (req, res) =>
     res.redirect('/')
 );
 
-const handleListen = () => console.log('Listening on http:');
-app.listen(3000, handleListen);
+const handleListen = () => console.log('Listening on http://localhost:3000');
+
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+    sockets.push(socket);
+    console.log("브라우저 연결 완료");
+    socket.on('close', () => console.log('서버 연결끊김'));
+
+    // socket.on('message', (message, isBinary)=> {
+    //     const messageString = isBinary? message : message.toString();
+    //     socket.send(messageString);
+    // });
+
+    socket.on('message', (message, isBinary)=> {
+        const messageString = isBinary? message : message.toString();
+        sockets.forEach((aSocket) => {
+            aSocket.send(messageString);
+        });
+    });
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
